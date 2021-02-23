bd1f52aedc7391463c84ed59e23b4e598f12a9c9
Gavin <h8r1dx4@gmail.com>
2021-02-23 11:14:47 +0200

    added file and base code...

---
+++ bin/enabled/traceroute_plugin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#mqtta version 1
+
+class traceroute:
+    
+    def __init__(self):
+        pass
+
+def main(): #temp
+    print('No load me as a plugin!')
+
+if __name__ == '__main__':
+    main()
+else:
+    pass
